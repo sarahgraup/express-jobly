25efbaffe515445e1036148dff06994f76f0d743
sarah <sarahgraup@g.ucla.edu>
2023-04-12 11:58:09 -0700

    testing for helper function

---
+++ helpers/sql.test.js
@@ -0,0 +1,25 @@
+const { BadRequestError } = require("../expressError");
+const { sqlForPartialUpdate } = require("./sql");
+
+describe("sqlForPartialUpdate", function () {
+    test("works with one value update", function () {
+        const result = sqlForPartialUpdate(
+            { numEmployees: 5 },
+            { numEmployees: "num_employees" }
+        );
+        expect(result).toEqual({
+            setCols: `"num_employees"=$1`,
+            values: [5]
+        });
+
+    });
+    test("throws error if no data", function () {
+
+        expect(() => {
+            (sqlForPartialUpdate({}))
+        }).toThrow(new BadRequestError("No data"));
+    });
+
+
+
+})
