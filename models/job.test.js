cac41c7333b2f1e31ed503ce3f71e954e93e8a05
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-14 14:15:09 -0400

    add imports for tests

---
+++ models/job.test.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const {
+  NotFoundError,
+  BadRequestError,
+  UnauthorizedError,
+} = require("../expressError");
+const db = require("../db.js");
+const Job = require("./job.js");
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+} = require("./_testCommon");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+
